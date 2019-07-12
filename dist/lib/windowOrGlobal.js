d19b4c319ccb9e8476ce9c584f99674d7aad01ba
Matt <winescout@gmail.com>
2019-07-12 13:07:15 -0700

    fixes minification error

--- dist/lib/windowOrGlobal.js
+++ dist/lib/windowOrGlobal.js
@@ -13,11 +13,11 @@
 })(this, function (module) {
   'use strict';
 
-  var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
+  var _mytypeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
     return typeof obj;
   } : function (obj) {
     return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
   };
 
-  module.exports = (typeof self === 'undefined' ? 'undefined' : _typeof(self)) === 'object' && self.self === self && self || (typeof global === 'undefined' ? 'undefined' : _typeof(global)) === 'object' && global.global === global && global || undefined;
+  module.exports = (typeof self === 'undefined' ? 'undefined' : _mytypeof(self)) === 'object' && self.self === self && self || (typeof global === 'undefined' ? 'undefined' : _mytypeof(global)) === 'object' && global.global === global && global || undefined;
 });
